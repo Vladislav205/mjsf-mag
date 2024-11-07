a1b65f4caa5e7f97a6822391917860a812c17ca7
Vladislav205 <51053326+Vladislav205@users.noreply.github.com>
2024-11-07 23:12:42 +0200

    Add files via upload

---
+++ lab4/src/App.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import Navigation from "@/components/Navigation.vue";
+</script>
+
+<script>
+import OptionsComponent from './OptionsComponent.vue';
+import CompositionComponent from './CompositionComponent.vue';
+
+export default {
+  data() {
+    return {
+      data: ''
+    };
+  },
+  mounted() {
+    this.serviceProvider = this.$serviceProvider.getData();
+  },
+  components: {
+    OptionsComponent,
+    CompositionComponent
+  }
+}
+</script>
+
+<template>
+  <header>
+    <Navigation />
+  </header>
+  <RouterView />
+
+  <OptionsComponent />
+  <CompositionComponent />
+
+  <button v-tooltip="{ 
+    content: 'Tooltip', 
+    html: false,
+    delay: 200, 
+    trigger: 'hover click', 
+    show: true 
+  }">
+    Click to hover
+  </button>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ lab4/src/main.ts
@@ -0,0 +1,26 @@
+import '@/assets/main.scss'
+
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
+import directivesPlugin from '@/plugins/directives';
+import i18n from '@/plugins/i18n'
+import DemoI18n from "@/DemoI18n.vue";
+import router from './router'
+import ServiceProviderPlugin from './plugins/service-provider';
+import tooltip from './plugins/tooltip';
+
+const app = createApp(DemoI18n)
+
+app.use(ServiceProviderPlugin)
+app.directive('tooltip', tooltip)
+
+app.use(createPinia())
+app.use(router)
+app.use(directivesPlugin);
+app.use(i18n, {
+    greetings: {
+        hi: 'Привіт!'
+    }
+});
+
+app.mount('#app')
